38185bb996e916509f278ef06fb65507a1fa4903
Zahra Rafiei <z.rafiei.82@gmail.com>
2023-09-17 09:48:06 -1200

    added costume Vazir font

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   return (
     <>
+      <h3>اپلیکیشن مدیریت مخاطبین</h3>
       <span className="fa fa-user"></span>
       <button className="btn btn-primary">btn</button>
     </>
